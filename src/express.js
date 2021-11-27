b70a13607e9bfa30143c6271a19c8b3f2edfd30c
francocuervoo <77750393+francocuervoo@users.noreply.github.com>
2021-11-27 17:00:56 -0300

    Update express.js

--- src/express.js
+++ src/express.js
@@ -93,81 +93,13 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-const PORT = 8080;
+const port = process.env.PORT || 3030;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(port, () => {
   console.log(
-    `Servidor express corriendo en el puerto http://localhost:${PORT}`
+    `Servidor express corriendo en el puerto http://localhost:${port}`
   );
 });
 
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
 
-
-
-
-
-
-
-
-
-
-
-
-// Commands
-// npm run start
-// npx eslint --init | configuro el eslint 2 / 2 / 3 / No / No / 3
-
-//All the products
-// app.get("/api/productos", async (req, res) => {
-//   let productos = await objetoContenedor.getAll();
-//   res.status(200).send(productos);
-// });
-
-//A product by id
-// app.get("/api/productos/:id", async (req, res) => {
-//   const { id } = req.params;
-//   let productoId = await objetoContenedor.getById(id);
-//   try {
-//     //productoId me devuelve undefined
-//     if (productoId) {
-//       res.status(200).send(productoId);
-//     } else {
-//       res.status(400).json({ error: "Producto no encontrado" });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// Receives and adds a product, and returns it with its assigned id
-// app.post("/api/productos", async (req, res) => {
-//   // No funciona, no se como hacer para que en la clase me quede el objeto con las propiedades correctas: title, price, thumbnail, id.
-//   const { product } = req.body;
-//   const productSave = await objetoContenedor.save(product);
-//   res.status(200).send(productSave);
-// });
-
-// Receives and update a product according the id
-// app.put("/api/productos/:id", async (req, res) => {
-//   // No se me ocurre como armar la función de la clase que recibe y actualiza su producto según su id.
-//   const { id } = req.body;
-//   const productSave = await objetoContenedor.updateById(id);
-//   res.status(200).send(productSave);
-// });
-
-// Delete a product according the id
-// app.delete("/api/productos/:id", async (req, res) => {
-//   const { id } = req.params;
-//   let productoId = await objetoContenedor.deleteById(id);
-//   try {
-//     // Funciona pero siempre devuelve "Producto no encontrado", ya que el productoId siempre me devuelve undefined
-//     if (productoId) {
-//       res.status(200).send(productoId);
-//     } else {
-//       res.status(400).json({ error: "Producto no encontrado" });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
